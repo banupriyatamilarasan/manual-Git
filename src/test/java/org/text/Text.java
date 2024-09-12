28f1dbe533e8d8721c0fac7cc64adbf6efdfde93
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-12 11:04:14 +0530

    no changes

--- src/test/java/org/text/Text.java
+++ src/test/java/org/text/Text.java
@@ -1,5 +1,16 @@
 package org.text;
 
 public class Text {
+	private void studentName() {
+		System.out.println("banu");
+	}
+	private void studentId() {
+		System.out.println("tamil");
+
+	}
+	private void schoolName() {
+System.out.println("Arul");
+	}
+	
 
 }
